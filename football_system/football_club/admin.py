d3228f2ae820d6eaa8b7c17a1c76aab6773a9391
Brian Miano <brianmiano1@gmail.com>
2025-08-19 17:55:25 +0100

    I have added a progress.md file where I will be stating what I have done daily procedurally so showcase the steps I have taken in developing this project. I have also registered the models from models.py to the admin.py file. This is important for it provides a way to have a functional data entry interface for the entire database without needing to work on the views and HTML templates yet. Next step should be to create the supepruser to access the Django admin interface

--- football_system/football_club/admin.py
+++ football_system/football_club/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Team,  Player, Match, Income, Expenses, Equipment, Assignment, TrainingSession
 
 # Register your models here.
+
+
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Match)
+admin.site.register(Income)
+admin.site.register(Expenses)
+admin.site.register(Equipment)
+admin.site.register(Assignment)
+admin.site.register(TrainingSession)
---
+++ football_system/football_club/progress.md
@@ -0,0 +1,5 @@
+Here I'll document the steps I will be taking during the project's completion from the time I started up to completion. 
+
+August 19th 2025
+
+--> Today I handled models.py and the admin.py file where I will be registering the different models
